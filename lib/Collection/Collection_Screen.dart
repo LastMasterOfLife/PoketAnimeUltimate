5a2083857f68bb9aa66a4e335c29d0fccb30d7c8
AndreaConrado <conrado.andrea@gmail.com>
2024-12-21 15:20:49 +0100

    implementazione Poket caricato con API correttamente

--- lib/Collection/Collection_Screen.dart
+++ lib/Collection/Collection_Screen.dart
@@ -1,5 +1,7 @@
-import 'package:poketanime/Home/Card_exaple.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:poketanime/Home/Card_exaple.dart';
 
 class CollectionScreen extends StatefulWidget {
   const CollectionScreen({super.key});
@@ -9,22 +11,36 @@ class CollectionScreen extends StatefulWidget {
 }
 
 class _CollectionScreenState extends State<CollectionScreen> {
-  // Lista di immagini e stati di blocco
-  final List<Map<String, dynamic>> items = [
-    {'image': 'assets/KaisenPack/cards/comune/backgrounds/gojo_card.jpg', 'locked': false},
-    {'image': 'assets/KaisenPack/cards/comune/backgrounds/toji_fushiguro_card.jpg', 'locked': false},
-    {'image': 'assets/KaisenPack/cards/comune/backgrounds/yuuji_itadori_card.jpg', 'locked': false},
-    {'image': 'assets/KaisenPack/cards/comune/backgrounds/nanami_card.jpg', 'locked': false},
-    {'image': 'assets/KaisenPack/cards/comune/backgrounds/rika.jpg', 'locked': false},
-    {'image': 'assets/KaisenPack/cards/comune/backgrounds/nobara_kugisaki.jpg', 'locked': false},
-    {'image': 'assets/KaisenPack/cards/comune/backgrounds/aoi_todo.jpg', 'locked': false},
-    {'image': 'assets/KaisenPack/cards/comune/backgrounds/megumi_fushiguro.jpg', 'locked': false},
-    {'image': 'assets/KaisenPack/cards/comune/backgrounds/Yuta & Rika.jpg', 'locked': true},
-    {'image': 'assets/KaisenPack/cards/comune/backgrounds/hanami.jpg', 'locked': false},
-    {'image': 'assets/KaisenPack/cards/comune/backgrounds/mahito.jpg', 'locked': true},
-    {'image': 'assets/KaisenPack/cards/comune/backgrounds/jogo.jpg', 'locked': true},
+  List<Map<String, dynamic>> items = [];
+  bool isLoading = true;
 
-  ];
+  Future<void> fetchCards() async {
+    final url = Uri.parse('https://mocki.io/v1/a41e7a76-0694-48ed-8943-2a9dfce772dc');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      final cardsList = data['cards'] as List<dynamic>;
+
+      setState(() {
+        items = cardsList.map((item) {
+          return {
+            'image': item['Immagine_sfondo'] ?? '',
+            'locked': item['locked'] ?? true, // Aggiungi una chiave 'locked' se disponibile
+          };
+        }).toList();
+        isLoading = false;
+      });
+    } else {
+      throw Exception('Errore nel recupero delle carte');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCards();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,19 +50,21 @@ class _CollectionScreenState extends State<CollectionScreen> {
     return Scaffold(
       extendBodyBehindAppBar: true,
       extendBody: true,
-      body: Padding(
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Padding(
         padding: const EdgeInsets.all(8.0),
         child: GridView.builder(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 3, // 3 container per riga
             crossAxisSpacing: 9.0, // Spazio orizzontale tra i container
             mainAxisSpacing: 20.0, // Spazio verticale tra i container
-            childAspectRatio: itemWidth / itemHeight
+            childAspectRatio: itemWidth / itemHeight,
           ),
           itemCount: items.length,
           itemBuilder: (context, index) {
             final item = items[index];
-            return gridItem(item['image'], item['locked'], itemHeight,itemWidth,index);
+            return gridItem(item['image'], item['locked'], itemHeight, itemWidth, index);
           },
         ),
       ),
@@ -68,12 +86,29 @@ class _CollectionScreenState extends State<CollectionScreen> {
             InkWell(
               onTap: () {
                 if (!locked) {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => CardExample(index: index,)));
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CardExample(index: index)),
+                  );
                 }
               },
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(10),
-                child: Image.asset(
+                child: imagePath.startsWith('http')
+                    ? Image.network(
+                  imagePath,
+                  fit: BoxFit.cover,
+                  height: double.infinity,
+                  width: double.infinity,
+                  loadingBuilder: (context, child, loadingProgress) {
+                    if (loadingProgress == null) return child;
+                    return const Center(child: CircularProgressIndicator());
+                  },
+                  errorBuilder: (context, error, stackTrace) {
+                    return const Icon(Icons.broken_image, color: Colors.red);
+                  },
+                )
+                    : Image.asset(
                   imagePath,
                   fit: BoxFit.cover,
                   height: double.infinity,
@@ -100,5 +135,4 @@ class _CollectionScreenState extends State<CollectionScreen> {
       ),
     );
   }
-
 }
