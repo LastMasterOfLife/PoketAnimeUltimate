d4b155f20f37fd5522e663af191b5269568c11f6
AndreaConrado <conrado.andrea@gmail.com>
2024-12-24 11:40:10 +0100

    installazione patch 3 di ladybug funzionante

--- lib/Loading/Loading_screen.dart
+++ lib/Loading/Loading_screen.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:poketanime/CustomerScaffold/CustomerScaffold_screen.dart';
+//import 'package:audioplayers/audioplayers.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -10,6 +11,8 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  //late AudioPlayer player = AudioPlayer();
   void navigazione(){
     // Naviga automaticamente alla pagina di Login dopo 3 secondi
     Future.delayed(const Duration(seconds: 5), () {
@@ -20,11 +23,27 @@ class _LoadingScreenState extends State<LoadingScreen> {
     });
   }
 
+  /*
+  void _playMusic() async {
+    await player.play(AssetSource('assets/Audio/caricamento.mp3'));
+  }
+
+   */
   @override @override
   void initState() {
     super.initState();
+    //_playMusic();
     navigazione();
   }
+
+  /*
+  @override
+  void dispose() {
+    player.stop();
+    super.dispose();
+  }
+
+   */
 
   @override
   Widget build(BuildContext context) {
