f91c7338b1cd2eff2b3ffac857a1541ce8c544f4
AndreaConrado <conrado.andrea@gmail.com>
2025-01-04 17:09:10 +0100

    implementazione di miglioramento UI

--- lib/Home/Card_exaple.dart
+++ lib/Home/Card_exaple.dart
@@ -2,10 +2,12 @@ import 'dart:convert'; // Per lavorare con JSON
 import 'package:flutter/material.dart';
 import 'package:flutter_tilt/flutter_tilt.dart';
 import 'package:http/http.dart' as http;
+import 'package:poketanime/Colors.dart';
 import 'package:poketanime/Componets/Card_Detail_Component.dart';
 
 class CardExample extends StatefulWidget {
   final int index;
+
   const CardExample({super.key, required this.index});
 
   @override
@@ -23,14 +25,16 @@ class _CardExampleState extends State<CardExample> {
 
   // Funzione per ottenere i dati della carta specifica
   Future<Map<String, dynamic>> fetchCardData(int id) async {
-    const String apiUrl = "https://mocki.io/v1/e1635e8e-c11a-48a5-b355-51bc60f10a12";
+    const String apiUrl =
+        "https://mocki.io/v1/e1635e8e-c11a-48a5-b355-51bc60f10a12";
     try {
       final response = await http.get(Uri.parse(apiUrl));
 
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
         final cards = data['cards'] as List;
-        return cards.firstWhere((card) => card['Id'] == id, orElse: () => throw Exception("Carta non trovata"));
+        return cards.firstWhere((card) => card['Id'] == id,
+            orElse: () => throw Exception("Carta non trovata"));
       } else {
         throw Exception("Errore durante la richiesta: ${response.statusCode}");
       }
@@ -57,7 +61,7 @@ class _CardExampleState extends State<CardExample> {
 
     return List.generate(
       starCount,
-          (index) => const Icon(
+      (index) => const Icon(
         Icons.star,
         color: Colors.amber,
         size: 24,
@@ -104,141 +108,102 @@ class _CardExampleState extends State<CardExample> {
                   child: CardDetailComponent(card: card),
                 ),
                 const SizedBox(height: 10),
-                Text(
-                  card['Nome'] ?? 'Sconosciuto',
-                  style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: buildStars(card['Rarita'] ?? ''),
-                ),
-                const SizedBox(height: 10),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    card['Descrizione'] ?? '',
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(fontSize: 16),
+                Container(
+                  height: 270,
+                  width: 350,
+                  decoration: BoxDecoration(
+                      color: primary,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20))),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 30),
+                        Text(
+                          card['Nome'] ?? 'Sconosciuto',
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: buildStars(card['Rarita'] ?? ''),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: bianco.withOpacity(0.7),
+                              borderRadius: BorderRadius.all(Radius.circular(15))
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 20),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const SizedBox(height: 10),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      card['Descrizione'] ?? '',
+                                      textAlign: TextAlign.center,
+                                      style: const TextStyle(fontSize: 16,color: grigio),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+
+                                  Text(
+                                    "Abilità: ${card['abilities'] ?? ''}",
+                                    style: const TextStyle(
+                                        fontSize: 16, fontWeight: FontWeight.bold,color: grigio),
+                                  ),
+
+                                  const SizedBox(height: 20),
+
+                                  Text("Attacco: ${card['attack'] ?? ''}",
+                                      style: const TextStyle(fontSize: 16,color: grigio)),
+
+                                  const SizedBox(height: 20),
+
+                                  Text("Difesa: ${card['defense'] ?? ''}",
+                                      style: const TextStyle(fontSize: 16,color: grigio)),
+
+                                  const SizedBox(height: 20),
+
+                                  Text("Velocità: ${card['speed'] ?? ''}",
+                                      style: const TextStyle(fontSize: 16 ,color: grigio)),
+
+                                  const SizedBox(height: 20),
+
+                                  Row(
+                                    children: [
+                                      const Icon(Icons.bolt, color: Colors.yellow, size: 24),
+                                      Text(
+                                        "${card['energy']}",
+                                        style: const TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: grigio,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+
+                                  const SizedBox(height: 20),
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
+                )
               ],
             ),
           );
         },
-      ),
-    );
-  }
-
-  Widget buildCard(Map<String, dynamic> card) {
-    return Tilt(
-      child: Container(
-        width: 300,
-        height: 450,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          image: DecorationImage(
-            image: AssetImage(buildBorder(card['Rarita'])),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Container(
-          margin: const EdgeInsets.all(10), // Spazio per il bordo
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            color: Colors.transparent,
-          ),
-          child: Stack(
-            children: [
-              Positioned.fill(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(15),
-                  child: Image.asset(
-                    card['Immagine_sfondo'] ?? '',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                child: TiltParallax(
-                  size: const Offset(20, 20),
-                  child: Image.asset(
-                    card['Immagine_personaggio'] ?? '',
-                    scale: 0.05,
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 10,
-                left: 15,
-                right: 15,
-                child: Center(
-                  child: Text(
-                    card['Nome'] ?? '',
-                    style: const TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      shadows: [
-                        Shadow(
-                          offset: Offset(1, 1),
-                          color: Colors.black,
-                          blurRadius: 3,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 40,
-                left: 15,
-                right: 15,
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.8),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Abilità: ${card['abilities'] ?? ''}",
-                        style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(height: 5),
-                      Text("Attacco: ${card['attack'] ?? ''}", style: const TextStyle(fontSize: 16)),
-                      Text("Difesa: ${card['defense'] ?? ''}", style: const TextStyle(fontSize: 16)),
-                      Text("Velocità: ${card['speed'] ?? ''}", style: const TextStyle(fontSize: 16)),
-                    ],
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 10,
-                right: 15,
-                child: Row(
-                  children: [
-                    const Icon(Icons.bolt, color: Colors.yellow, size: 24),
-                    Text(
-                      "${card['energy'] ?? ''}",
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
       ),
     );
   }
