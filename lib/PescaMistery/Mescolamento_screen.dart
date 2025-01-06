2fcaf06b3197a762a0f444dd3b8e1ca2fa5f5bb5
AndreaConrado <conrado.andrea@gmail.com>
2025-01-06 13:07:05 +0100

    ottimizzazione codice

--- lib/PescaMistery/Mescolamento_screen.dart
+++ lib/PescaMistery/Mescolamento_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:poketanime/Componets/Animated_Card_Component.dart';
 import 'package:poketanime/Componets/Pesca_Component.dart';
 import 'package:poketanime/CustomerScaffold/CustomerScaffold_screen.dart';
-import 'package:poketanime/PescaMistery/Mescolamento_screen.dart';
 
 class MescolamentoScreen extends StatefulWidget {
   final List<Map<String, dynamic>> pesca;
@@ -20,7 +19,6 @@ class _MescolamentoScreenState extends State<MescolamentoScreen> {
   void initState() {
     super.initState();
 
-    // Inizializza lo stato delle carte come non ruotate
     isFlipped = List<bool>.filled(widget.pesca.length, false);
 
     // Ruota tutte le carte dopo 1.5 secondi
@@ -88,42 +86,4 @@ class _MescolamentoScreenState extends State<MescolamentoScreen> {
     );
   }
 }
-/*
-class AnimatedCard extends StatelessWidget {
-  final bool isFlipped;
-  final Widget frontWidget;
 
-  const AnimatedCard({
-    Key? key,
-    required this.isFlipped,
-    required this.frontWidget,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TweenAnimationBuilder(
-      tween: Tween<double>(begin: 0, end: isFlipped ? 1 : 0),
-      duration: const Duration(milliseconds: 800),
-      builder: (context, value, child) {
-        final angle = value * 3.1416; // Ruota di 180°
-        return Transform(
-          alignment: Alignment.center,
-          transform: Matrix4.rotationY(angle),
-          child: value < 0.5
-              ? frontWidget // Mostra il fronte
-              : Container(
-            decoration: const BoxDecoration(
-              color: Colors.brown,
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-            ),
-            child: const Center(
-              child: Text(""),
-            ),
-          ),
-        );
-      },
-    );
-  }
-}
-
- */
