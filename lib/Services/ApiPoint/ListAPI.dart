b859b4293acbdbb7c679a796fb846c976fb4dabe
LastMasterOfLife <102365168+LastMasterOfLife@users.noreply.github.com>
2026-01-21 10:59:55 +0100

    aggiornamento lista API 21_01_2026

--- lib/Services/ApiPoint/ListAPI.dart
+++ lib/Services/ApiPoint/ListAPI.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-const ListPack = "https://mocki.io/v1/42c14f94-1847-44e8-9629-38b40918b6cd"; // lista 3 pack
-const DemonSlayerPack = "https://mocki.io/v1/16236884-c734-44db-860c-1c6ad908b932"; // lista Demon Slayer
-const JujutsuKaisenPack = "https://mocki.io/v1/242b73d1-7a3e-4f58-83f0-741a583d0060"; // lista Jujutsu Kaisen
-const ListMissionDay = "https://mocki.io/v1/a869c11c-532c-4f31-bdd1-52ac250594e1"; // lista Mission Day
-const ListMissionCardex = "https://mocki.io/v1/e9130261-080c-4ae6-a415-eb88133da4f4"; // lista Mission Cardex
-const ListMissionEmblema = "https://mocki.io/v1/6287ed92-7810-44db-8d6b-efc343ed98e4"; // lista Mission Emblema
+const ListPack = "https://mocki.io/v1/be336f30-06cb-438f-a9fa-55458ff9b335"; // lista 3 pack
+const DemonSlayerPack = "https://mocki.io/v1/539f081f-9da2-4457-a46b-d4b304b4484d"; // lista Demon Slayer
+const JujutsuKaisenPack = "https://mocki.io/v1/4bea7c90-93c4-4a40-8acb-8cd12c67fa1c"; // lista Jujutsu Kaisen
+const ListMissionDay = "https://mocki.io/v1/15069d1a-7188-40a9-8106-0ccbf18229a5"; // lista Mission Day
+const ListMissionCardex = "https://mocki.io/v1/0658643c-d57d-44ab-907b-3b59f1f89ccb"; // lista Mission Cardex
+const ListMissionEmblema = "https://mocki.io/v1/ca8c7ab1-54c4-4d0e-8f80-53f4e13ef1d8"; // lista Mission Emblema
 
-const MysteryPackJK = "https://mocki.io/v1/242b73d1-7a3e-4f58-83f0-741a583d0060"; // lista Pesca Misteriosa di Jujutsu Kaisen
-const mysteryPackDS = "https://mocki.io/v1/16236884-c734-44db-860c-1c6ad908b932"; // lista Pesca Misteriosa di Demon Slayer
+const MysteryPackJK = "https://mocki.io/v1/4bea7c90-93c4-4a40-8acb-8cd12c67fa1c"; // lista Pesca Misteriosa di Jujutsu Kaisen
+const mysteryPackDS = "https://mocki.io/v1/539f081f-9da2-4457-a46b-d4b304b4484d"; // lista Pesca Misteriosa di Demon Slayer
